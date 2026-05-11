cd0804c58aa938e70b6632eb9fedc7719a388397
KosiOriginal <kosibgthegamer@gmail.com>
2026-05-11 15:57:18 +0300

    idk what is that

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+
+
+
+
 const btn = document.getElementById("Dark_light");
 
+
+//main theme changer
 btn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
+
+
+
+//CHANGING BETWEEN DARK AND LIGHT IN THE BUTTON
+btn.addEventListener("click", () => {
+    if (btn.textContent === "LIGHT") {
+        btn.textContent = "DARK";
+    } else {
+        btn.textContent = "LIGHT";
+    }
+});
+
+
+
