0982362e905dd264a2e47de2df8c6e4e518a4372
KosiOriginal <kosibgthegamer@gmail.com>
2026-05-13 20:06:46 +0300

    Dark theme button upgrade

--- Konstantin/index.js
+++ Konstantin/index.js
@@ -1,25 +1,17 @@
-
-
-
-
 const btn = document.getElementById("Dark_light");
+const toggleIcon = document.getElementById("toggle-icon");
 
+if (btn && toggleIcon) {
+    btn.addEventListener("click", () => {
+        document.body.classList.toggle("dark");
 
-//main theme changer
-btn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-});
+        const isDark = document.body.classList.contains("dark");
 
+        if (isDark) {
+            toggleIcon.src = 'pngs/sun.png';
+        } else {
+            toggleIcon.src = 'pngs/moon.png';
+        }
 
-
-//CHANGING BETWEEN DARK AND LIGHT IN THE BUTTON
-btn.addEventListener("click", () => {
-    if (btn.textContent === "LIGHT") {
-        btn.textContent = "DARK";
-    } else {
-        btn.textContent = "LIGHT";
-    }
-});
-
-
-
+    });
+}
